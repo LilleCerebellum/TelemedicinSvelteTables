a8b53b1b804931e96ae487a46fe77c303d652306
LilleCerebellum <Soellingvraa72@gmail.com>
2025-05-05 15:15:46 +0200

    update patient page

--- LektionSeje/src/routes/+page.svelte
+++ LektionSeje/src/routes/+page.svelte
@@ -1,2 +1,22 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+	import { goto } from "$app/navigation";
+
+    let brugernavn = $state("");
+    let password = $state("");
+
+    const loginButton = () => {
+        if (brugernavn === "Morten" && password === "Morten") {
+            goto('/home')
+
+        } else if (brugernavn === "Rasmus" && password === "Rasmus") {
+            window.open("https://maps.app.goo.gl/qcViKmE1Rud1AVjr9")
+        }
+        
+        else { alert("nuhuh") } 
+    }
+</script>
+
+<input class="input m-2" til bind:value={brugernavn} placeholder="Brugernavn">
+<input class="input m-2" til bind:value={password} placeholder="Password">
+<button class="btn btn-primary m-2" onclick={loginButton}> Login </button>
+
