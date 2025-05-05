b83e0cb6c1e3e51d9dec0fe944fbbf4561fa5c9a
LilleCerebellum <Soellingvraa72@gmail.com>
2025-05-05 15:13:17 +0200

    Admin page created

--- LektionSeje/src/lib/server/db/schema.js
+++ LektionSeje/src/lib/server/db/schema.js
@@ -1,6 +1,9 @@
+import type { USERNAME } from '$env/static/private';
+
 import { pgTable, serial, text, integer } from 'drizzle-orm/pg-core';
 
 export const user = pgTable('user', {
 	id: serial('id').primaryKey(),
-	age: integer('age')
+	USERNAME: text('username').notNull().unique(),
+	password: text('password').notNull()
 });
---
+++ LektionSeje/src/routes/admin/+page.svelte
@@ -0,0 +1,36 @@
+<script>
+    import { createSubscriber } from "svelte/reactivity";
+
+    let username = $state('user');
+    let password = $state('password');
+
+
+    const createUser = async ( ) => {
+        const response = await fetch(' /api/user', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ username, password})
+        });
+        await response.json();
+        if (response.ok) {
+            alert('User created')
+        } else {
+            alert('error: user note created')
+        }
+        username = '';
+        password = '';
+    };
+</script>
+
+<h1 class="text-2x1 font-bold"> Kun for seje mennesker, jeg er sej med store gonader</h1>
+<div>
+    <input type="text" bind:value={username}, placeholder="Brugernavn", class="input" />
+</div>
+<div>
+    <input type="text" bind:value={password}, placeholder="password", class="input" />
+</div>
+<div>
+    <button class="btn btn-primary" onclick="createUser">Opret bruger</button>
+</div>
