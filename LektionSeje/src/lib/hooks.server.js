ad04fa0487c9b1079c7e4f91b6844d766a09ffc9
LilleCerebellum <Soellingvraa72@gmail.com>
2025-05-09 08:24:17 +0200

    Update hooks.server.js

---
+++ LektionSeje/src/lib/hooks.server.js
@@ -0,0 +1,24 @@
+import { env } from '$env/dynamic/private';
+import jwt from 'jsonwebtoken'; // Import JWT library
+
+
+export async function handle({ event, resolve }) {
+    console.log(event.url.pathname);
+    const token = event.cookies.get('session');
+    if (['/', '/login', '/api/login'].includes(event.url.pathname)) {
+        return await resolve(event);
+    }
+    if (token) {
+        try {
+            // Verify the token and extract user information
+            const decoded = jwt.verify(token, env.JWT_SECRET);
+            event.request.user = decoded; // store user for later
+            return await resolve(event); // Proceed to route
+        } catch (err) {
+            console.error('Token verification failed:', err);
+        }
+    }
+    //Reject the request if the token is not present or invalid
+    return new Response(JSON.stringify({ error: 'Unauthorized' }), { status: 401 });
+}
+
