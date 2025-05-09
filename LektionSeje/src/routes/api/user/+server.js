89d8b75e3510f97938cffd1cb99cb22c7801813b
LilleCerebellum <Soellingvraa72@gmail.com>
2025-05-09 07:36:15 +0200

    i beg the lord man

--- LektionSeje/src/routes/api/user/+server.js
+++ LektionSeje/src/routes/api/user/+server.js
@@ -8,6 +8,6 @@ export async function POST({ request, cookies }) {
     const { username, password } = await request.json();
     const hashedPass = await bcrypt.hash(password, 10);
     const createduser = await db.insert(user).values({ username, password: hashedPass }).returning()
-    return new Response(json.stringify(createduser), { status: 201});
+    return new Response(JSON.stringify(createduser), { status: 201});
 
 }
