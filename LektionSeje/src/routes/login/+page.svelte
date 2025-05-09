14c5531f2a5f6e91e1a829a6ab9531b46f18f840
LilleCerebellum <Soellingvraa72@gmail.com>
2025-05-09 18:50:22 +0200

    Update +page.svelte

--- LektionSeje/src/routes/login/+page.svelte
+++ LektionSeje/src/routes/login/+page.svelte
@@ -17,8 +17,27 @@
             alert('Login fejlet!');
         }
     };
+
+    const loginPatient = async () => {
+        const response = await fetch('/api/Patientlogin', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ username, password })
+        });
+        const data = await response.json();
+        if (response.ok) {
+            goto('/admin');
+        } else {
+            alert('Login fejlet!');
+        }
+    };
+
+
 </script>
 <input bind:value={username} type="text" placeholder="username" />
 <input bind:value={password} type="password" placeholder="password" />
-<button onclick={login}>Login</button>
+<button onclick={login}>Login Læge</button>
 
+<input bind:value={username} type="text" placeholder="username" />
+<input bind:value={password} type="password" placeholder="password" />
+<button onclick={loginPatient}>Login Patient</button>
