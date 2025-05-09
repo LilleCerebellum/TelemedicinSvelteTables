abea0733e0eba71ab532a53b5913e05513f20a8b
LilleCerebellum <Soellingvraa72@gmail.com>
2025-05-09 06:27:19 +0200

    try again

--- LektionSeje/src/routes/api/user/+server.js
+++ LektionSeje/src/routes/api/user/+server.js
@@ -1,4 +1,4 @@
-/*
+
 import bcrypt from "bcryptjs";
 import { user } from '$lib/server/db/Schema.js';
 import { db } from '$lib/server/db';
@@ -10,22 +10,4 @@ export async function POST({ request, cookies }) {
     const createduser = await db.insert(user).values({ username, password: hashedPass }).returning()
     return new Response(json.stringify(createduser), { status: 201});
 
-} */
-
-import { db } from '$lib/server/db';
-import { user } from '$lib/server/db/schema.js';
-
-export async function POST({ request }) {
-    const { username, password } = await request.json();
-
-    // Dynamic import to avoid Vite/Rollup bundling issues
-    const bcrypt = await import('bcryptjs');
-
-    const hashedPass = await bcrypt.hash(password, 10); // Hash password
-    const createdUser = await db.insert(user).values({
-        username,
-        password: hashedPass
-    }).returning();
-
-    return new Response(JSON.stringify(createdUser), { status: 201 });
 }
